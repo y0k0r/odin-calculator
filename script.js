489a4ee6cd2e7d9d9db3abd99813ba2ad851aed5
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-04-12 19:43:59 -0400

    feat: add operate function
    - Implement 'operate' function with a switch statement
    - Routes to the appropriate math operation based on the operator

--- script.js
+++ script.js
@@ -12,11 +12,25 @@ function multiply (a, b) {
   return a * b;
 }
 
-
 function divide (a , b) {
   if (b === 0) {
     return `Error`;
   } else {
     return a / b;
+  }
+}
+
+function operate (a, b, operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return null;
   }
 }
