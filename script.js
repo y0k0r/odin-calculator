9d79430aeb5a0e9b0a08d85f93df45890621f358
Yoko <73258869+y0k0r@users.noreply.github.com>
2025-04-12 22:05:57 -0400

    feat: add event listeners for numeric buttons
    - Appended numeric value to each button's class
    - Added query selectors for all numeric buttons
    - Attached event listeners to each button to call populateDisplay with the correct number
    - Added 'defer' attribute to script tag to ensure DOM loads before script runs
    - Initialized currentInput as an empty string to fix 'undefined' issue

--- script.js
+++ script.js
@@ -35,10 +35,34 @@ function operate (a, b, operator) {
   }
 }
 
-let currentInput;
+let currentInput = "";
 
 function populateDisplay(input) {
   const output = document.querySelector(".display-area output");
   currentInput += input;
   output.innerText = currentInput;
 }
+
+
+const btn0 = document.querySelector(".btn-numeric-0");
+const btn1 = document.querySelector(".btn-numeric-1");
+const btn2 = document.querySelector(".btn-numeric-2");
+const btn3 = document.querySelector(".btn-numeric-3");
+const btn4 = document.querySelector(".btn-numeric-4");
+const btn5 = document.querySelector(".btn-numeric-5");
+const btn6 = document.querySelector(".btn-numeric-6");
+const btn7 = document.querySelector(".btn-numeric-7");
+const btn8 = document.querySelector(".btn-numeric-8");
+const btn9 = document.querySelector(".btn-numeric-9");
+
+btn0.addEventListener("click", () => populateDisplay("0"));
+btn1.addEventListener("click", () => populateDisplay("1"));
+btn2.addEventListener("click", () => populateDisplay("2"));
+btn3.addEventListener("click", () => populateDisplay("3"));
+btn4.addEventListener("click", () => populateDisplay("4"));
+btn5.addEventListener("click", () => populateDisplay("5"));
+btn6.addEventListener("click", () => populateDisplay("6"));
+btn7.addEventListener("click", () => populateDisplay("7"));
+btn8.addEventListener("click", () => populateDisplay("8"));
+btn9.addEventListener("click", () => populateDisplay("9"));
+
